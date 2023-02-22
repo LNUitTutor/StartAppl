86b2839c1841a6627760e2365c5290a99caf9074
LNUitTutor <lnu.it.tutor@gmail.com>
2023-02-22 14:38:14 +0200

    Add project files.

---
+++ StartAppl/FirstForm.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StartAppl
+{
+    public partial class FirstForm : Form
+    {
+        public FirstForm()
+        {
+            InitializeComponent();
+        }
+
+        private void exit_button_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show(
+                $"Чи справді Ви хочете завершити програму?",
+                "Підтвердження",
+                MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+            if (result == DialogResult.OK) Close();
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            NumericUpDown numeric = sender as NumericUpDown;
+            switch ((int)numeric.Value)
+            {
+                case 1: label2.Text = "Не з'явився"; break;
+                case 2: label2.Text = "Незадовільно"; break;
+                case 3: label2.Text = "Задовільно"; break;
+                case 4: label2.Text = "Добре"; break;
+                case 5: label2.Text = "Відмінно"; break;
+            }
+        }
+    }
+}
